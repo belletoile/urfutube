552d07b37b5c48f7fbd47f42580c11baa3cc6256
evgenia <kordeugenie@yandex.ru>
2023-06-11 20:42:20 +0500

    fix

--- services/files.py
+++ services/files.py
@@ -1,6 +1,6 @@
 import os
 
-from fastapi import UploadFile, File
+from fastapi import File, UploadFile
 from loguru import logger
 
 from services.Storage import Storage
@@ -8,10 +8,9 @@ from services.Storage import Storage
 
 def save_video(file: UploadFile = File(...)):
     try:
-        content = file.file.read()
-
         with open('tmp_files/' + file.filename, 'wb') as f:
-            f.write(content)
+            while contents := file.file.read(1024 * 1024):
+                f.write(contents)
 
         s3 = Storage('38cfe289-urfutube')
         file_url = s3.save_file(file.filename)
